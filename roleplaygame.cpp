024e2d0371836714d2443b21de0a84a12c2f9516
dermoritz123 <moritz-hartlieb@web.de>
2013-10-25 20:08:41 +0200

    Szenario hizugefuegt! Erstes mal getestet

--- roleplaygame.cpp
+++ roleplaygame.cpp
@@ -1,4 +1,41 @@
 #include <iostream>
-#include <stdio>
+#include <cstdio>
+#include <string>
+using namespace std;
+
+static string txtWald = "Das Haus gefaellt dir nicht also gehst du wieder in den  dunklen Wald... Die Eulen rufen, die Woelfe heulen und du siehst zwei Augen im Gebuesch. Was ist das fragst du dich , doch da kommt es schon heraus gesprungen und greift dich an. Ein Wolf!!! Was tust du? [W]: Weglaufen [V]: Dein Messer nehmen und dich verteidigen";
+static 
+void SzeneWald(){
+	
+	cout << txtWald<<endl;
+	
+	
+}
+
+void SzeneEingang(){
+	
+	//cout << txtEingang<<endl;
+	
+	
+}
+
+void SzeneHaus(){
+	//cout << txtHaus << endl;
+	char select;
+	select = getchar();
+	switch(select){
+		case 'W':
+			SzeneWald();
+		case 'E':
+			SzeneEingang();
+	}
+	
+	
+}
 
 
+
+int main(){
+	SzeneHaus();
+	return 0;
+}
