e6307e854ff84d85e764f0415e620e7ea1a9f9d6
dermoritz123 <moritz-hartlieb@web.de>
2013-10-25 20:50:51 +0200

    geändert:   roleplaygame.cpp

--- roleplaygame.cpp
+++ roleplaygame.cpp
@@ -8,7 +8,9 @@ void SzeneLichtung();
 
 static string txtWald = "Das Haus gefaellt dir nicht also gehst du wieder in den  dunklen Wald... Die Eulen rufen, die Woelfe heulen und du siehst zwei Augen im Gebuesch. Was ist das fragst du dich , doch da kommt es schon heraus gesprungen und greift dich an. Ein Wolf!!! Was tust du? [W]: Weglaufen [V]: Dein Messer nehmen und dich verteidigen";
 
-static string txtLichtung = "Endlich bist du aus diesem dunklen gruseligen Wald heraus. Auf der Lichtung vor dir steht ein heruntergekommenes Haus. Ob da noch jemand wohnt? Wahrscheinlich nicht. Was tust du? [E]: zum Eingang gehen [U]: das Haus umrunden ";
+static string txtLichtung = "Endlich bist du aus diesem dunklen gruseligen Wald heraus. Auf der Lichtung vor dir steht ein heruntergekommenes Haus. Ob da noch jemand wohnt? Wahrscheinlich nicht. Was tust du? [E]: zum Eingang gehen [U]: das Haus umrunden  [W]: zurück in den Wald gehen";
+
+static string txtEingang = "Okay. Du traust dich zum Eingang zu gehen. Doch dort siehst du das die Tür veriegelt ist. Was tust du? [Z]: zurueck gehen [A]: Tuer aufbrechen ";
 
 void SzeneHinten(){
 	//cout << txtHinten<<endl;
@@ -31,8 +33,15 @@ void SzeneWald(){
 
 void SzeneEingang(){
 	
-	//cout << txtEingang<<endl;
-	
+	cout << txtEingang<<endl;
+	char select;
+	cin >> select;
+	switch(select){
+		case 'Z':
+			SzeneLichtung();
+		case 'A':
+			SzeneTod();
+	}
 	
 }
 
